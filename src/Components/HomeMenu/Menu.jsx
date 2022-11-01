b1c97a29d723152c828f6ebfc8ecd1e85f79e24e
mshayat <saquibhayat26@gmail.com>
2022-11-01 19:57:27 +0530

    menu added

--- src/Components/HomeMenu/Menu.jsx
+++ src/Components/HomeMenu/Menu.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Menu.css";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      <div className="menu__wrapper">
+        <p>
+          Welcome to food's <br />
+          kitchen
+        </p>
+        <button>go to menu</button>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
